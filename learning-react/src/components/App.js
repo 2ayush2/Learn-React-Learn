5693fa810876b6d078d8211c835837bd63befb29
Ayush Khadka <80869490+2ayush2@users.noreply.github.com>
2023-08-03 22:11:11 +0545

    UseEffect

--- learning-react/src/components/App.js
+++ learning-react/src/components/App.js
@@ -1,15 +1,30 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const [contacts, setContact] = useState([]);
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContact] = useState(() => {
+    const storeContact = localStorage.getItem(LOCAL_STORAGE_KEY);
+    return storeContact ? JSON.parse(storeContact) : [];
+  });
   const contactHandler = (contact) => {
     setContact([...contacts, contact]);
     console.log(contact);
   };
+
+  // useEffect(() => {
+  //   const con = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  //   if (con) {
+  //     setContact(con);
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
   return (
     <div className="ui container">
       <Header />
